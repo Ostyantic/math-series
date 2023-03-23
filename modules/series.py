3ee2bf7bd7e0a0d5b001dbb3d9a9a24adda7dd0f
Anthony Lopez <Antl21292@gmail.com>
2023-03-22 21:25:01 -0400

    created base function with parameters

---
+++ modules/series.py
@@ -0,0 +1,27 @@
+def fibonacci(n):
+    """
+
+    :param n:
+    :return:
+    """
+    pass
+
+
+def lucas(n):
+    """
+
+    :param n:
+    :return:
+    """
+    pass
+
+
+def sum_series(n, start=0, add_to_start=1):
+    """
+
+    :param n:
+    :param start:
+    :param add_to_start:
+    :return:
+    """
+    pass
