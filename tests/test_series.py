75de19b30e16dc504edff4a389539872223312ff
Anthony Lopez <Antl21292@gmail.com>
2023-03-22 22:05:22 -0400

    Starter test to check if function exists

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,2 +1,18 @@
+from modules.series import fibonacci
 
+from modules.series import lucas
+
+from modules.series import sum_series
+
+
+def test_fibonacci_exists():
+    assert fibonacci(1)
+
+
+def test_lucas_exists():
+    assert lucas(1)
+
+
+def test_sum_series_exists():
+    assert sum_series(1)
 
